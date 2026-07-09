3049b4c53b0dde017abe5c1021662f11ed67af94
muqadasilyass <f2024se022@niit.edu.pk>
2026-07-09 13:54:51 +0500

    Prepare project for deployment

--- db.js
+++ db.js
@@ -2,14 +2,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root', // Replace with your actual DB user
-    password: 'HelloWorld@123', // Replace with your actual DB password
-    database: 'moviesystemdb', // Ensure this matches your database name
-    port:3308,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
     
 });
 
